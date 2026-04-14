0d16d4b2bd365c29cb08788efc5a33998a107d17
Pasya <tangleroad0101@gmail.com>
2026-04-14 14:28:03 +0800

    komit

--- MODUL 1 Struct dan Pointer/line.cpp
+++ MODUL 1 Struct dan Pointer/line.cpp
@@ -1,2 +1,16 @@
 #include "line.h"
+#include <cmath>
 
+double gradient(const Line * l, const Point * p) {
+    return l->a * p->x + l->b * p->y + l->c;
+}
+
+std::string CheckPointPosition(double gradient) {
+    if (fabs(gradient) < EPSILON) {
+        return "On Line";
+    } else if (gradient > 0) {
+        return "Left";
+    } else {
+        return "Right";
+    }
+}
--- MODUL 1 Struct dan Pointer/prob1.cpp
+++ MODUL 1 Struct dan Pointer/prob1.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
+#include "line.h"
 using namespace std;
 
-string CheckPointPosition(int a, int b, int c, int xP, int yP) {
-    int value = a * xP + b * yP + c;
-
-    if (value == 0) {
-        return "On Line";
-    } else if (value > 0) {
-        return "Left";
-    } else {
-        return "Right";
-    }
-}
-
 int main() {
-    int a, b, c, xP, yP;
-    cin >> a >> b >> c >> xP >> yP;
+    Line l;
+    Point p;
 
-    cout << CheckPointPosition(a, b, c, xP, yP);
+    cin >> l.a >> l.b >> l.c >> p.x >> p.y;
+
+    double val = gradient(&l, &p);
+    cout << CheckPointPosition(val) << endl;
 
     return 0;
 }
